122c9ba5b9dbd2a3bb534a38d0a3ef2dd3e2e8e8
Divi <sylvain.lorinet@gmail.com>
2014-05-09 19:25:24 +0200

    Moving "all_summoner_data_current_game" route into SummonerController.

--- src/EloGank/Api/Controller/SummonerController.php
+++ src/EloGank/Api/Controller/SummonerController.php
@@ -51,4 +51,76 @@ class SummonerController extends Controller
 
         return $this->call('summoner.summoner_by_name', [$summonerName]);
     }
+
+    /**
+     * Return all data needed to show information about a summoner current game
+     *
+     * @param int $accountId
+     * @param int $summonerId
+     *
+     * @return array
+     */
+    public function getAllSummonerDataCurrentGameAction($accountId, $summonerId)
+    {
+        $invokeIds = [
+            $this->getClient()->invoke('summonerService', 'getAllPublicSummonerDataByAccount', [$accountId], function ($result) {
+                foreach ($result['spellBook']['bookPages'] as $bookPage) {
+                    if (true === $bookPage['current']) {
+                        return ['spellBook' => $bookPage];
+                    }
+                }
+
+                return ['spellBook' => []];
+            }),
+            $this->getClient()->invoke('masteryBookService', 'getMasteryBook', [$summonerId], function ($result) {
+                foreach ($result['bookPages'] as $bookPage) {
+                    if (true === $bookPage['current']) {
+                        return ['masteryBook' => $bookPage];
+                    }
+                }
+
+                return ['masteryBook' => []];
+            }),
+            $this->getClient()->invoke('playerStatsService', 'getRecentGames', [$summonerId], function ($result) {
+                return ['recentGames' => $result['gameStatistics']];
+            }),
+            $this->getClient()->invoke('leaguesServiceProxy', 'getAllLeaguesForPlayer', [$summonerId], function ($result) use ($summonerId) {
+                foreach ($result['summonerLeagues'] as $summonerLeague) {
+                    if ('RANKED_SOLO_5x5' != $summonerLeague['queue']) {
+                        continue;
+                    }
+
+                    foreach ($summonerLeague['entries'] as $entry) {
+                        if ($summonerId == $entry['playerOrTeamId']) {
+                            return ['league' => [
+                                'name'  => $summonerLeague['name'],
+                                'queue' => $summonerLeague['queue'],
+                                'data'  => $entry
+                            ]];
+                        }
+                    }
+                }
+
+                return ['league' => []];
+            }),
+            $this->getClient()->invoke('playerStatsService', 'retrieveTopPlayedChampions', [$accountId, 'CLASSIC'], function ($result) {
+                if (!isset($result[0])) {
+                    return ['mainChampion' => null];
+                }
+
+                return ['mainChampion' => $result[1]['championId']];
+            }),
+        ];
+
+        $results = [];
+        foreach ($invokeIds as $invokeId) {
+            $result = $this->getResult($invokeId);
+            reset($result);
+            $key = key($result);
+
+            $results[$key] = $result[$key];
+        }
+
+        return $this->view($results);
+    }
 }
